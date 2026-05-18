eea1af3d9d5d3cb6b33684da4532d77ab9862d65
tigercodes-dev <266315912+tigercodes-dev@users.noreply.github.com>
2026-05-17 21:10:03 -0500

    Changed to use more advanced QEMU command with -drive instead of -cdrom.

--- debug.sh
+++ debug.sh
@@ -6,5 +6,5 @@ fi
 
 echo "Run 'gdb dist/x86_64-pkrnl.dbg' and connect to QEMU with 'target remote :1234'"
 
-qemu-system-x86_64 -s -S -cdrom dist/x86_64-popkernel.iso \
+qemu-system-x86_64 -s -S -drive file=dist/x86_64-popkernel.iso,media=cdrom,if=ide \
     -device isa-debugcon,iobase=0xe9,chardev=log -chardev stdio,id=log
--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ if [ ! -f dist/x86_64-popkernel.iso ]; then
     echo "You have to build the ISO before you can run it!"
 fi
 
-qemu-system-x86_64 -cdrom dist/x86_64-popkernel.iso \
+qemu-system-x86_64 -drive file=dist/x86_64-popkernel.iso,media=cdrom,if=ide \
     -device isa-debugcon,iobase=0xe9,chardev=log -chardev stdio,id=log
